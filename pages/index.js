28a0214aaa69b70d21fb948cd788948a829cdc74
Md Shamim <5222250-os-shamim@users.noreply.gitlab.com>
2021-04-27 01:00:57 +0600

    init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,56 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import React from 'react';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Home = ({photo}) => {
+    return (
+        <div>
+            <Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+                <title>Social Media Preview</title>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+                {/*<meta property="og:type" content="website"/>*/}
+                {/*<meta property="og:url" content="https://static.fembula.com"/>*/}
+                {/*<meta property="og:image" content={photo?.url}/>*/}
+                {/*<meta property="og:site_name" content="Social Media Preview"/>*/}
+                {/*<meta property="og:title" content={photo?.title}/>*/}
+                {/*<meta property="og:description" content="Hurray!! Yes Social Media Preview is Working"/>*/}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                {/*<meta property="fb:app_id" content="2747726002141483"/>*/}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                {/*<meta name="twitter:card" content="summary"/>*/}
+                {/*<meta name="twitter:creator" content="summary creator"/>*/}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                <meta property="og:title" content="Static Fembula" />
+                <meta property="og:description" content="Social preview Static Fembula" />
+                <meta property="og:image" content="https://cdn0.iconfinder.com/data/icons/line-file-type-icons/100/file_image_1-512.png" />
+                <meta property="og:url" content="https://static.fembula.com" />
+                <meta name="twitter:card" content="summary_large_image" />
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <meta property="og:site_name" content="Fembula, Inc." />
+                <meta name="twitter:image:alt" content="Alt text for image" />
+
+                <meta property="fb:app_id" content="507733220387321" />
+                <meta name="twitter:site" content="@website-username" />
+
+            </Head>
+            <h2>{photo?.title}</h2>
+            <img src={photo?.url} alt="Alt image"/>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    );
 }
+export default Home
+
+export const getServerSideProps = async () => {
+    let photo = null;
+    await fetch('https://jsonplaceholder.typicode.com/photos/1')
+        .then((response) => response.json())
+        .then((json) => {
+            photo = json
+        })
+
+    return {
+        props: {
+            photo,
+        },
+    };
+};
