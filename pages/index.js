3c63c6e5eb85dba4d639b6b60b4287d4625772fd
Md Shamim <5222250-os-shamim@users.noreply.gitlab.com>
2021-04-27 01:12:46 +0600

    final preview

--- pages/index.js
+++ pages/index.js
@@ -8,29 +8,20 @@ const Home = ({photo}) => {
 
                 <title>Social Media Preview</title>
 
-                {/*<meta property="og:type" content="website"/>*/}
-                {/*<meta property="og:url" content="https://static.fembula.com"/>*/}
-                {/*<meta property="og:image" content={photo?.url}/>*/}
-                {/*<meta property="og:site_name" content="Social Media Preview"/>*/}
-                {/*<meta property="og:title" content={photo?.title}/>*/}
-                {/*<meta property="og:description" content="Hurray!! Yes Social Media Preview is Working"/>*/}
+                <meta property="og:type" content="website"/>
 
-                {/*<meta property="fb:app_id" content="2747726002141483"/>*/}
+                <meta property="og:title" content={photo?.title}/>
+                <meta property="og:description" content="Social media preview"/>
+                <meta property="og:image" content={photo?.url}/>
+                <meta property="og:url" content="https://static.fembula.com"/>
+                <meta name="twitter:card" content="summary_large_image"/>
 
-                {/*<meta name="twitter:card" content="summary"/>*/}
-                {/*<meta name="twitter:creator" content="summary creator"/>*/}
+                <meta property="og:site_name" content="Social Media, Inc."/>
+                <meta name="twitter:image:alt" content="Alt text for image"/>
 
-                <meta property="og:title" content="Static Fembula" />
-                <meta property="og:description" content="Social preview Static Fembula" />
-                <meta property="og:image" content="https://cdn0.iconfinder.com/data/icons/line-file-type-icons/100/file_image_1-512.png" />
-                <meta property="og:url" content="https://static.fembula.com" />
-                <meta name="twitter:card" content="summary_large_image" />
-
-                <meta property="og:site_name" content="Fembula, Inc." />
-                <meta name="twitter:image:alt" content="Alt text for image" />
-
-                <meta property="fb:app_id" content="507733220387321" />
-                <meta name="twitter:site" content="@website-username" />
+                <meta property="fb:app_id" content="507733220387321"/>
+                <meta name="twitter:site" content="@website-username"/>
+                <meta name="twitter:creator" content="summary creator"/>
 
             </Head>
             <h2>{photo?.title}</h2>
